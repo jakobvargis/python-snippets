174eaa8b74163ea94bfaff1ae66f9404bec60f75
Jakob Vargis <103443890+jakobvargis@users.noreply.github.com>
2025-08-19 20:00:35 +0530

    Create 01snippets.py

---
+++ 01snippets.py
@@ -0,0 +1,21 @@
+# Testing for python fastapi programning
+def get_full_name(first_name: str, last_name: str, age: int):
+    full_name = f"{first_name.title()} {last_name.title()}, he is {age} years old!." 
+    return full_name
+    
+
+print(get_full_name("jakob", "vargis", 50))
+
+
+def items_list(*args: list):
+    for item in args:
+        print(f"{item} x 2 == {item * 2}")
+        
+
+items_list(1,2,3,4,5,6)
+
+def str_or_int(item: int | str):
+    print(f"Data: {item}")
+    
+
+str_or_int(3.9)
